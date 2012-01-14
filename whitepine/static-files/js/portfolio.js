b146e13018474f7c56554fc565f53065db8081e1
Jake Trent <trent.jake@gmail.com>
2012-01-14 14:33:45 -0700

    Advance thru the photos

--- whitepine/static-files/js/portfolio.js
+++ whitepine/static-files/js/portfolio.js
@@ -1,12 +1,17 @@
 var DisplayView = Backbone.View.extend({
   el: '#display',
+  events: {
+    'click .arrow.left': 'left',
+    'click .arrow.right': 'right'
+  },
   initialize: function () {
     _.bindAll(this);
     Backbone.Events.bind('photosDisplay', this.renderPhotos);
   },
   renderPhotos: function (photos) {
     this.photos = photos;
-    $(this.el).find('.disp-photo').remove();
+    this.index = 0;
+    $(this.el).children('.disp-photo').remove();
     var template = _.template($('#photo-display').html());
     var frag = document.createDocumentFragment();
     _(photos).each(function (photo) {
@@ -14,6 +19,28 @@ var DisplayView = Backbone.View.extend({
     });
     $(this.el).append(frag);
     return this;
+  },
+  showPhoto: function () {
+    var $photos = $(this.el).children('.disp-photo');
+    $photos.hide();
+    $photos.eq(this.index).show();
+  },
+  left: function () {
+    this.moveIndex(-1);
+    this.showPhoto();
+  },
+  right: function () {
+    this.moveIndex(1);
+    this.showPhoto();
+  },
+  moveIndex: function (move) {
+    if (this.index + move == this.photos.length) {
+      this.index = 0;
+    } else if (this.index + move < 0) {
+      this.index = this.photos.length - 1;
+    } else {
+      this.index = this.index + move;
+    }
   }
 });
 
