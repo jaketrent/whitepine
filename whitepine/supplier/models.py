6203fe997a71319b1188b83bd2ed7092cb48cfde
Jake Trent <trent.jake@gmail.com>
2012-01-14 19:07:27 -0700

    remove link field

--- whitepine/supplier/models.py
+++ whitepine/supplier/models.py
@@ -13,7 +13,6 @@ class Category(models.Model):
 class Supplier(models.Model):
   title = models.CharField(max_length=200)
   title_slug = models.SlugField(unique=True)
-  link = models.CharField(max_length=500, default='http://google.com')
   category = models.ForeignKey(Category)
   image = models.CharField(max_length=500, blank=True, null=True)
   description = models.TextField(max_length=1000, blank=True, null=True)
