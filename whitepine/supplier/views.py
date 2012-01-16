1cfee230ad74eb583ee267b3c98aafdc0833b989
Jake Trent <trent.jake@gmail.com>
2012-01-16 11:12:41 -0700

    show list of suppliers

--- whitepine/supplier/views.py
+++ whitepine/supplier/views.py
@@ -3,6 +3,8 @@ from django.template import RequestContext
 from whitepine.supplier.models import Supplier, Category
 
 def list(request):
-  suppliers = Supplier.objects.all()
-  categories = Category.objects.all()
+  suppliers = Supplier.objects.all().order_by('title')
+  categories = Category.objects.all().order_by('title')
+  if (len(categories) > 0):
+    first_cat = categories[0]
   return render_to_response("supplier.html", locals(), context_instance=RequestContext(request))
